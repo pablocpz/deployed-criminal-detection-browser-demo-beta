5bd21e8626d85be8e4977c892a4ba4546d806290
Pablo Cobo <pablocpzz.ai@gmail.com>
2024-06-26 04:49:01 +0200

    allow all cors

--- backend/sam_server/app.py
+++ backend/sam_server/app.py
@@ -411,5 +411,4 @@ async def root():
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000,
-                ssl_keyfile="certificates.pem", ssl_certfile="certificates.pem")
+    uvicorn.run(app, host="0.0.0.0", port=8000)
